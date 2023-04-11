670f6a15b413cf652996c39096323258ab478aca
sosdatpapku <114177169+sosdatpapku@users.noreply.github.com>
2023-04-11 22:02:52 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import shapiro, ttest_ind
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1188007817 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    control_group = x
+    experimental_group = y
+    p_value_control = shapiro(control_group)
+    p_value_experimental = shapiro(experimental_group)
+    t_statistic, p_value = ttest_ind(control_group, experimental_group)
+    alpha = 0.06
+    if p_value < alpha:
+        retun True
+    else:
+        return False
